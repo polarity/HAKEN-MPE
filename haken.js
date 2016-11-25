c95d0134a3397370471ebd4c6a3963e2318cae53
Robert Agthe <robert@scriptshit.de>
2016-11-25 19:04:13 +0100

    first cleanup

--- haken.js
+++ haken.js
@@ -3,7 +3,7 @@ loadAPI(1)
 host.defineController('Haken', 'Haken Board', '1.0', '8a064e50-b330-11e6-9598-0800200c9a66')
 host.defineMidiPorts(1, 1)
 
-function init () {
+function init() {
   host.getMidiInPort(0).setMidiCallback(onMidi)
   noteInput = host.getMidiInPort(0).createNoteInput('',
     '8?????',
@@ -31,50 +31,20 @@ function init () {
   sendPitchBendRangeRPN(1, 48)
 
   device = host.createEditorCursorDevice()
-
-  device.getMacro(0).getAmount().addValueObserver(128, function (value) {
-    setSliderValueLED(0, value)
-  })
-
-  device.getMacro(1).getAmount().addValueObserver(128, function (value) {
-    setSliderValueLED(1, value)
-  })
-
-  device.getMacro(2).getAmount().addValueObserver(128, function (value) {
-    setSliderValueLED(2, value)
-  })
-
-  for (var i = 0; i < 8; i++) {
-    device.getMacro(i).getModulationSource().addIsMappedObserver(getMacroIsMappedFunc(i))
-  }
 }
 
-function getMacroIsMappedFunc (index) {
-  return function (value) {
-    macroIsMapped[index] = value
-  }
-}
-
-function setSliderValueLED (slider, value) {
-  sendSysex('F0 00 21 10 78 3D ' + uint7ToHex(20 + slider) + uint7ToHex(Math.max(11, value)) + ' F7')
-}
-
-function sendPitchBendRangeRPN (channel, range) {
+function sendPitchBendRangeRPN(channel, range) {
   sendChannelController(channel, 100, 0); // Registered Parameter Number (RPN) - LSB*
   sendChannelController(channel, 101, 0); // Registered Parameter Number (RPN) - MSB*
   sendChannelController(channel, 36, 0)
   sendChannelController(channel, 6, range)
 }
 
-function sendMacroIfMappedOtherWiseAsMidi (macro, status, data1, data2) {
-  if (macroIsMapped[macro]) {
-    device.getMacro(macro).getAmount().set(data2, 128)
-  }else {
-    noteInput.sendRawMidiEvent(status, data1, data2)
-  }
+function sendMacroIfMappedOtherWiseAsMidi(macro, status, data1, data2) {
+  noteInput.sendRawMidiEvent(status, data1, data2)
 }
 
-function onMidi (status, data1, data2) {
+function onMidi(status, data1, data2) {
   // printMidi(status, data1, data2)
 
   if (status == 176) {
@@ -88,7 +58,6 @@ function onMidi (status, data1, data2) {
       case 111:
         sendMacroIfMappedOtherWiseAsMidi(2, status, data1, data2)
         break
-
       case 113:
         sendMacroIfMappedOtherWiseAsMidi(4, status, data1, data2)
         break
@@ -99,5 +68,5 @@ function onMidi (status, data1, data2) {
   }
 }
 
-function exit () {
+function exit() {
 }
